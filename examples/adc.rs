4eb4617aa6afbd40692c11802a3226406e344e59
Gasper <gasper@gmx.com>
2020-03-15 15:26:27 +0100

    ADC: interface draft example

---
+++ examples/adc.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+//use xtensa_lx6_rt as _;
+#[macro_use(block)]
+extern crate nb;
+
+use core::panic::PanicInfo;
+
+use embedded_hal::adc::OneShot as _;
+
+use esp32;
+use esp32_hal::adc::{config::Config, ADC, ADC1};
+use esp32_hal::gpio::GpioExt;
+
+
+#[no_mangle]
+fn main() -> ! {
+    let dp = unsafe { esp32::Peripherals::steal() };
+    let all_pins = dp.GPIO.split();
+    let mut pin36 = all_pins.gpio36.into_floating_input();
+
+    let mut adc1 = ADC::adc1(Config::default()).unwrap();
+
+    loop {
+        let raw_value: u16 = block!(adc1.read(&mut pin36)).unwrap();
+    }
+}
+
+/// Basic panic handler - just loops
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
