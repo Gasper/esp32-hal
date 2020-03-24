e848ccf3f830f396353b2cfddc44ed843cdcbeeb
Gasper <gasper@gmx.com>
2020-03-24 09:59:37 +0100

    Replaced disable_analog! macro with a function call

--- src/gpio.rs
+++ src/gpio.rs
@@ -140,22 +140,10 @@ gpio! {
    ]
 }
 
-macro_rules! disable_analog {
-    ($self:ident, has_analog) => {
-        $self.disable_analog();
-    };
-    ($self:ident, no_analog) => {
-        /* Analog/RTC functionality not present on this pin */
-    };
-    ($self:ident, $has_analog:ident) => {
-        compile_error!("The GPIO pin has to be mark with either has_analog or no_analog.");
-    };
-}
-
 macro_rules! impl_output {
     ($en:ident, $outs:ident, $outc:ident, [
         // index, gpio pin name, funcX name, iomux pin name
-        $($pxi:ident: ($i:expr, $pin:ident, $funcXout:ident, $iomux:ident, $has_analog:ident),)+
+        $($pxi:ident: ($i:expr, $pin:ident, $funcXout:ident, $iomux:ident),)+
     ]) => {
         $(
             impl<MODE> OutputPin for $pxi<Output<MODE>> {
@@ -201,7 +189,7 @@ macro_rules! impl_output {
                 pub fn into_push_pull_output(self) -> $pxi<Output<PushPull>> {
                     let gpio = unsafe{ &*GPIO::ptr() };
                     let iomux = unsafe{ &*IO_MUX::ptr() };
-                    disable_analog!(self, $has_analog);
+                    self.disable_analog();
 
                     gpio.$en.modify(|_, w| unsafe  { w.bits(0x1 << $i) });
                     gpio.$funcXout.modify(|_, w| unsafe { w.bits(0x100) });
@@ -215,7 +203,7 @@ macro_rules! impl_output {
                 pub fn into_open_drain_output(self) -> $pxi<Output<OpenDrain>> {
                     let gpio = unsafe{ &*GPIO::ptr() };
                     let iomux = unsafe{ &*IO_MUX::ptr() };
-                    disable_analog!(self, $has_analog);
+                    self.disable_analog();
 
                     gpio.$en.modify(|_, w| unsafe  { w.bits(0x1 << $i) });
                     gpio.$funcXout.modify(|_, w| unsafe { w.bits(0x100) });
@@ -229,7 +217,7 @@ macro_rules! impl_output {
                 fn set_alternate(&self, n: u8) {
                     let gpio = unsafe{ &*GPIO::ptr() };
                     let iomux = unsafe{ &*IO_MUX::ptr() };
-                    disable_analog!(self, $has_analog);
+                    self.disable_analog();
 
                     gpio.$en.modify(|_, w| unsafe  { w.bits(0x1 << $i) });
                     gpio.$funcXout.modify(|_, w| unsafe { w.bits(0x100) });
@@ -270,51 +258,51 @@ macro_rules! impl_output {
 
 impl_output! {
     enable_w1ts, out_w1ts, out_w1tc, [
-        Gpio0: (0, pin0, func0_out_sel_cfg, gpio0, has_analog),
-        Gpio1: (1, pin1, func1_out_sel_cfg, u0txd, no_analog),
-        Gpio2: (2, pin2, func2_out_sel_cfg, gpio2, has_analog),
-        Gpio3: (3, pin3, func3_out_sel_cfg, u0rxd, no_analog),
-        Gpio4: (4, pin4, func4_out_sel_cfg, gpio4, has_analog),
-        Gpio5: (5, pin5, func5_out_sel_cfg, gpio5, no_analog),
-        Gpio6: (6, pin6, func6_out_sel_cfg, sd_clk, no_analog),
-        Gpio7: (7, pin7, func7_out_sel_cfg, sd_data0, no_analog),
-        Gpio8: (8, pin8, func8_out_sel_cfg, sd_data1, no_analog),
-        Gpio9: (9, pin9, func9_out_sel_cfg, sd_data2, no_analog),
-        Gpio10: (10, pin10, func10_out_sel_cfg, sd_data3, no_analog),
-        Gpio11: (11, pin11, func11_out_sel_cfg, sd_cmd, no_analog),
-        Gpio12: (12, pin12, func12_out_sel_cfg, mtdi, has_analog),
-        Gpio13: (13, pin13, func13_out_sel_cfg, mtck, has_analog),
-        Gpio14: (14, pin14, func14_out_sel_cfg, mtms, has_analog),
-        Gpio15: (15, pin15, func15_out_sel_cfg, mtdo, has_analog),
-        Gpio16: (16, pin16, func16_out_sel_cfg, gpio16, no_analog),
-        Gpio17: (17, pin17, func17_out_sel_cfg, gpio17, no_analog),
-        Gpio18: (18, pin18, func18_out_sel_cfg, gpio18, no_analog),
-        Gpio19: (19, pin19, func19_out_sel_cfg, gpio19, no_analog),
-        Gpio20: (20, pin20, func20_out_sel_cfg, gpio20, no_analog),
-        Gpio21: (21, pin21, func21_out_sel_cfg, gpio21, no_analog),
-        Gpio22: (22, pin22, func22_out_sel_cfg, gpio22, no_analog),
-        Gpio23: (23, pin23, func23_out_sel_cfg, gpio23, no_analog),
-        Gpio25: (25, pin25, func25_out_sel_cfg, gpio25, has_analog),
-        Gpio26: (26, pin26, func26_out_sel_cfg, gpio26, has_analog),
-        Gpio27: (27, pin27, func27_out_sel_cfg, gpio27, has_analog),
+        Gpio0: (0, pin0, func0_out_sel_cfg, gpio0),
+        Gpio1: (1, pin1, func1_out_sel_cfg, u0txd),
+        Gpio2: (2, pin2, func2_out_sel_cfg, gpio2),
+        Gpio3: (3, pin3, func3_out_sel_cfg, u0rxd),
+        Gpio4: (4, pin4, func4_out_sel_cfg, gpio4),
+        Gpio5: (5, pin5, func5_out_sel_cfg, gpio5),
+        Gpio6: (6, pin6, func6_out_sel_cfg, sd_clk),
+        Gpio7: (7, pin7, func7_out_sel_cfg, sd_data0),
+        Gpio8: (8, pin8, func8_out_sel_cfg, sd_data1),
+        Gpio9: (9, pin9, func9_out_sel_cfg, sd_data2),
+        Gpio10: (10, pin10, func10_out_sel_cfg, sd_data3),
+        Gpio11: (11, pin11, func11_out_sel_cfg, sd_cmd),
+        Gpio12: (12, pin12, func12_out_sel_cfg, mtdi),
+        Gpio13: (13, pin13, func13_out_sel_cfg, mtck),
+        Gpio14: (14, pin14, func14_out_sel_cfg, mtms),
+        Gpio15: (15, pin15, func15_out_sel_cfg, mtdo),
+        Gpio16: (16, pin16, func16_out_sel_cfg, gpio16),
+        Gpio17: (17, pin17, func17_out_sel_cfg, gpio17),
+        Gpio18: (18, pin18, func18_out_sel_cfg, gpio18),
+        Gpio19: (19, pin19, func19_out_sel_cfg, gpio19),
+        Gpio20: (20, pin20, func20_out_sel_cfg, gpio20),
+        Gpio21: (21, pin21, func21_out_sel_cfg, gpio21),
+        Gpio22: (22, pin22, func22_out_sel_cfg, gpio22),
+        Gpio23: (23, pin23, func23_out_sel_cfg, gpio23),
+        Gpio25: (25, pin25, func25_out_sel_cfg, gpio25),
+        Gpio26: (26, pin26, func26_out_sel_cfg, gpio26),
+        Gpio27: (27, pin27, func27_out_sel_cfg, gpio27),
     ]
 }
 
 impl_output! {
     enable1_w1ts, out1_w1ts, out1_w1tc, [
-        Gpio32: (0, pin32, func32_out_sel_cfg, gpio32, has_analog),
-        Gpio33: (1, pin33, func33_out_sel_cfg, gpio33, has_analog),
+        Gpio32: (0, pin32, func32_out_sel_cfg, gpio32),
+        Gpio33: (1, pin33, func33_out_sel_cfg, gpio33),
         /* Deliberately omitting 34-39 as these can *only* be inputs */
     ]
 }
 
 macro_rules! impl_pullup_pulldown {
     ($en:ident, $pxi:ident, $i:expr, $funcXin:ident,
-        $iomux:ident, $has_analog:ident, has_pullup_pulldown) => {
+        $iomux:ident, has_pullup_pulldown) => {
         pub fn into_pull_up_input(self) -> $pxi<Input<PullUp>> {
             let gpio = unsafe{ &*GPIO::ptr() };
             let iomux = unsafe{ &*IO_MUX::ptr() };
-            disable_analog!(self, $has_analog);
+            self.disable_analog();
 
             gpio.$en.modify(|_, w| unsafe  { w.bits(0x1 << $i) });
             gpio.$funcXin.modify(|_, w| unsafe { w.bits(0x100) });
@@ -329,7 +317,7 @@ macro_rules! impl_pullup_pulldown {
         pub fn into_pull_down_input(self) -> $pxi<Input<PullDown>> {
             let gpio = unsafe{ &*GPIO::ptr() };
             let iomux = unsafe{ &*IO_MUX::ptr() };
-            disable_analog!(self, $has_analog);
+            self.disable_analog();
 
             gpio.$en.modify(|_, w| unsafe  { w.bits(0x1 << $i) });
             gpio.$funcXin.modify(|_, w| unsafe { w.bits(0x100) });
@@ -342,11 +330,11 @@ macro_rules! impl_pullup_pulldown {
         }
     };
     ($en:ident, $pxi:ident, $i:expr, $funcXin:ident,
-        $iomux:ident, $has_analog:ident, no_pullup_pulldown) => {
+        $iomux:ident, no_pullup_pulldown) => {
         /* No pullup/pulldown resistor is available on this pin. */
     };
     ($en:ident, $pxi:ident, $i:expr, $funcXin:ident,
-        $iomux:ident, $has_analog:ident, $pullup_flag:ident) => {
+        $iomux:ident, $pullup_flag:ident) => {
         compile_error! ("The GPIO pin has to be marked with either \
             has_pullup_pulldown or no_pullup_pulldown.");
     };
@@ -356,8 +344,7 @@ macro_rules! impl_input {
     ($en:ident, $reg:ident, $reader:ident [
         // index, gpio pin name, funcX name, iomux pin name,
         // has pullup/down resistors, has analog functionality
-        $($pxi:ident: ($i:expr, $pin:ident, $funcXin:ident, $iomux:ident,
-            $resistors:ident, $has_analog:ident),)+
+        $($pxi:ident: ($i:expr, $pin:ident, $funcXin:ident, $iomux:ident, $resistors:ident),)+
     ]) => {
         $(
             impl<MODE> InputPin for $pxi<Input<MODE>> {
@@ -376,7 +363,7 @@ macro_rules! impl_input {
                 pub fn into_floating_input(self) -> $pxi<Input<Floating>> {
                     let gpio = unsafe{ &*GPIO::ptr() };
                     let iomux = unsafe{ &*IO_MUX::ptr() };
-                    disable_analog!(self, $has_analog);
+                    self.disable_analog();
 
                     gpio.$en.modify(|_, w| unsafe  { w.bits(0x1 << $i) });
                     gpio.$funcXin.modify(|_, w| unsafe { w.bits(0x100) });
@@ -388,7 +375,7 @@ macro_rules! impl_input {
                     $pxi { _mode: PhantomData }
                 }
 
-                impl_pullup_pulldown! ($en, $pxi, $i, $funcXin, $iomux, $has_analog, $resistors);
+                impl_pullup_pulldown! ($en, $pxi, $i, $funcXin, $iomux, $resistors);
             }
         )+
     };
@@ -396,47 +383,62 @@ macro_rules! impl_input {
 
 impl_input! {
     enable_w1ts, in_, in_data [
-        Gpio0: (0, pin0, func0_in_sel_cfg, gpio0, has_pullup_pulldown, has_analog),
-        Gpio1: (1, pin1, func1_in_sel_cfg, u0txd, has_pullup_pulldown, no_analog),
-        Gpio2: (2, pin2, func2_in_sel_cfg, gpio2, has_pullup_pulldown, has_analog),
-        Gpio3: (3, pin3, func3_in_sel_cfg, u0rxd, has_pullup_pulldown, no_analog),
-        Gpio4: (4, pin4, func4_in_sel_cfg, gpio4, has_pullup_pulldown, has_analog),
-        Gpio5: (5, pin5, func5_in_sel_cfg, gpio5, has_pullup_pulldown, no_analog),
-        Gpio6: (6, pin6, func6_in_sel_cfg, sd_clk, has_pullup_pulldown, no_analog),
-        Gpio7: (7, pin7, func7_in_sel_cfg, sd_data0, has_pullup_pulldown, no_analog),
-        Gpio8: (8, pin8, func8_in_sel_cfg, sd_data1, has_pullup_pulldown, no_analog),
-        Gpio9: (9, pin9, func9_in_sel_cfg, sd_data2, has_pullup_pulldown, no_analog),
-        Gpio10: (10, pin10, func10_in_sel_cfg, sd_data3, has_pullup_pulldown, no_analog),
-        Gpio11: (11, pin11, func11_in_sel_cfg, sd_cmd, has_pullup_pulldown, no_analog),
-        Gpio12: (12, pin12, func12_in_sel_cfg, mtdi, has_pullup_pulldown, has_analog),
-        Gpio13: (13, pin13, func13_in_sel_cfg, mtck, has_pullup_pulldown, has_analog),
-        Gpio14: (14, pin14, func14_in_sel_cfg, mtms, has_pullup_pulldown, has_analog),
-        Gpio15: (15, pin15, func15_in_sel_cfg, mtdo, has_pullup_pulldown, has_analog),
-        Gpio16: (16, pin16, func16_in_sel_cfg, gpio16, has_pullup_pulldown, no_analog),
-        Gpio17: (17, pin17, func17_in_sel_cfg, gpio17, has_pullup_pulldown, no_analog),
-        Gpio18: (18, pin18, func18_in_sel_cfg, gpio18, has_pullup_pulldown, no_analog),
-        Gpio19: (19, pin19, func19_in_sel_cfg, gpio19, has_pullup_pulldown, no_analog),
-        Gpio20: (20, pin20, func20_in_sel_cfg, gpio20, has_pullup_pulldown, no_analog),
-        Gpio21: (21, pin21, func21_in_sel_cfg, gpio21, has_pullup_pulldown, no_analog),
-        Gpio22: (22, pin22, func22_in_sel_cfg, gpio22, has_pullup_pulldown, no_analog),
-        Gpio23: (23, pin23, func23_in_sel_cfg, gpio23, has_pullup_pulldown, no_analog),
-        Gpio25: (25, pin25, func25_in_sel_cfg, gpio25, has_pullup_pulldown, has_analog),
-        Gpio26: (26, pin26, func26_in_sel_cfg, gpio26, has_pullup_pulldown, has_analog),
-        Gpio27: (27, pin27, func27_in_sel_cfg, gpio27, has_pullup_pulldown, no_analog),
+        Gpio0: (0, pin0, func0_in_sel_cfg, gpio0, has_pullup_pulldown),
+        Gpio1: (1, pin1, func1_in_sel_cfg, u0txd, has_pullup_pulldown),
+        Gpio2: (2, pin2, func2_in_sel_cfg, gpio2, has_pullup_pulldown),
+        Gpio3: (3, pin3, func3_in_sel_cfg, u0rxd, has_pullup_pulldown),
+        Gpio4: (4, pin4, func4_in_sel_cfg, gpio4, has_pullup_pulldown),
+        Gpio5: (5, pin5, func5_in_sel_cfg, gpio5, has_pullup_pulldown),
+        Gpio6: (6, pin6, func6_in_sel_cfg, sd_clk, has_pullup_pulldown),
+        Gpio7: (7, pin7, func7_in_sel_cfg, sd_data0, has_pullup_pulldown),
+        Gpio8: (8, pin8, func8_in_sel_cfg, sd_data1, has_pullup_pulldown),
+        Gpio9: (9, pin9, func9_in_sel_cfg, sd_data2, has_pullup_pulldown),
+        Gpio10: (10, pin10, func10_in_sel_cfg, sd_data3, has_pullup_pulldown),
+        Gpio11: (11, pin11, func11_in_sel_cfg, sd_cmd, has_pullup_pulldown),
+        Gpio12: (12, pin12, func12_in_sel_cfg, mtdi, has_pullup_pulldown),
+        Gpio13: (13, pin13, func13_in_sel_cfg, mtck, has_pullup_pulldown),
+        Gpio14: (14, pin14, func14_in_sel_cfg, mtms, has_pullup_pulldown),
+        Gpio15: (15, pin15, func15_in_sel_cfg, mtdo, has_pullup_pulldown),
+        Gpio16: (16, pin16, func16_in_sel_cfg, gpio16, has_pullup_pulldown),
+        Gpio17: (17, pin17, func17_in_sel_cfg, gpio17, has_pullup_pulldown),
+        Gpio18: (18, pin18, func18_in_sel_cfg, gpio18, has_pullup_pulldown),
+        Gpio19: (19, pin19, func19_in_sel_cfg, gpio19, has_pullup_pulldown),
+        Gpio20: (20, pin20, func20_in_sel_cfg, gpio20, has_pullup_pulldown),
+        Gpio21: (21, pin21, func21_in_sel_cfg, gpio21, has_pullup_pulldown),
+        Gpio22: (22, pin22, func22_in_sel_cfg, gpio22, has_pullup_pulldown),
+        Gpio23: (23, pin23, func23_in_sel_cfg, gpio23, has_pullup_pulldown),
+        Gpio25: (25, pin25, func25_in_sel_cfg, gpio25, has_pullup_pulldown),
+        Gpio26: (26, pin26, func26_in_sel_cfg, gpio26, has_pullup_pulldown),
+        Gpio27: (27, pin27, func27_in_sel_cfg, gpio27, has_pullup_pulldown),
     ]
 }
 
 impl_input! {
     enable1_w1ts, in1, in1_data [
-        Gpio32: (0, pin32, func32_in_sel_cfg, gpio32, has_pullup_pulldown, has_analog),
-        Gpio33: (1, pin33, func33_in_sel_cfg, gpio33, has_pullup_pulldown, has_analog),
-        Gpio34: (2, pin34, func34_in_sel_cfg, gpio34, no_pullup_pulldown, has_analog),
-        Gpio35: (3, pin35, func35_in_sel_cfg, gpio35, no_pullup_pulldown, has_analog),
-        Gpio36: (4, pin36, func36_in_sel_cfg, gpio36, no_pullup_pulldown, has_analog),
-        Gpio37: (5, pin37, func37_in_sel_cfg, gpio37, no_pullup_pulldown, has_analog),
-        Gpio38: (6, pin38, func38_in_sel_cfg, gpio38, no_pullup_pulldown, has_analog),
-        Gpio39: (7, pin39, func39_in_sel_cfg, gpio39, no_pullup_pulldown, has_analog),
+        Gpio32: (0, pin32, func32_in_sel_cfg, gpio32, has_pullup_pulldown),
+        Gpio33: (1, pin33, func33_in_sel_cfg, gpio33, has_pullup_pulldown),
+        Gpio34: (2, pin34, func34_in_sel_cfg, gpio34, no_pullup_pulldown),
+        Gpio35: (3, pin35, func35_in_sel_cfg, gpio35, no_pullup_pulldown),
+        Gpio36: (4, pin36, func36_in_sel_cfg, gpio36, no_pullup_pulldown),
+        Gpio37: (5, pin37, func37_in_sel_cfg, gpio37, no_pullup_pulldown),
+        Gpio38: (6, pin38, func38_in_sel_cfg, gpio38, no_pullup_pulldown),
+        Gpio39: (7, pin39, func39_in_sel_cfg, gpio39, no_pullup_pulldown),
     ]
+}
+
+macro_rules! impl_no_analog {
+    ([
+        $($pxi:ident),+
+    ]) => {
+        $(
+            impl<MODE> $pxi<MODE> {
+                #[inline(always)]
+                fn disable_analog(&self) {
+                    /* No analog functionality on this pin, so nothing to do */
+                }
+            }
+        )+
+    };
 }
 
 macro_rules! impl_analog {
@@ -479,6 +481,7 @@ macro_rules! impl_analog {
                     $pxi { _mode: PhantomData }
                 }
 
+                #[inline(always)]
                 fn disable_analog(&self) {
                     let rtcio = unsafe{ &*RTCIO::ptr() };
                     rtcio.$pin_reg.modify(|_,w| w.$mux_sel().clear_bit());
@@ -487,6 +490,11 @@ macro_rules! impl_analog {
         )+
     }
 }
+
+impl_no_analog! {[
+    Gpio1, Gpio3, Gpio5, Gpio6, Gpio7, Gpio8, Gpio9, Gpio10, Gpio11,
+    Gpio16, Gpio17, Gpio18, Gpio19, Gpio20, Gpio21, Gpio22, Gpio23
+]}
 
 impl_analog! {[
     Gpio36: (0, rtc_io_sensor_pads, rtc_gpio_pin0, rtc_io_sense1_mux_sel, rtc_io_sense1_fun_sel, rtc_gpio_pin0_pad_driver, rtc_io_sense1_fun_ie,),
